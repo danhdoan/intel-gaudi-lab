d751943abacf98ba9cd619550700714b842996d4
Nicolas Howard <29888087+nicolashoward@users.noreply.github.com>
2025-04-08 09:58:11 +0000

    WIP #12 - prepare `gaudi_spawn` for distributed inference - prepare distributed inference scripts

---
+++ scripts/run_deepspeed_inference.sh
@@ -0,0 +1,19 @@
+python experimentals/distributed_runner/gaudi_spawn.py \
+    --world_size 4 \
+    --use_deepspeed \
+    experimentals/sd_xl_base_1/sd_xl_base_1_inference.py \
+    --model_name_or_path "models/stable-diffusion-xl-base-1.0" \
+    --prompts "An image of falling leaves in an avenue in Monet style" \
+    --negative_prompts "tedious" \
+    --num_images_per_prompt 10 \
+    --batch_size 8 \
+    --num_inference_steps 30 \
+    --image_save_dir output/deepspeed/$(date +'%y%m%d_%H%M') \
+    --scheduler ddim \
+    --height 512 \
+    --width 512 \
+    --use_habana \
+    --use_hpu_graphs \
+    --gaudi_config Habana/stable-diffusion \
+    --sdp_on_bf16 \
+    --bf16
